5957bae12b2bd737c0f0e2c13483e5fa4b349239
cjh724 <jh.cho133104@gmail.com>
2025-10-14 00:19:33 +0900

    스프링 빈 등록 방법2 : 직접 스프링 빈 등록(+오타수정)

--- src/main/java/hello/springbootstart/repository/MemoryMemberRepository.java
+++ src/main/java/hello/springbootstart/repository/MemoryMemberRepository.java
@@ -1,11 +1,9 @@
 package hello.springbootstart.repository;
 
 import hello.springbootstart.domain.Member;
-import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long,Member> store = new HashMap<>();    // DB 가 아닌 메모리에 저장
     private static long sequence = 0L;
--- src/main/java/hello/springbootstart/service/MemberService.java
+++ src/main/java/hello/springbootstart/service/MemberService.java
@@ -1,43 +1,38 @@
 package hello.springbootstart.service;
 
 import hello.springbootstart.domain.Member;
-import hello.springbootstart.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import hello.springbootstart.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
-    private final MemoryMemberRepository memoryMemberRepository;
+    private final MemberRepository memberRepository;
 
-    @Autowired
-    public MemberService(MemoryMemberRepository memoryMemberRepository) {
-        // new 를 통해서 생성하는게 아니라 외부에서 넣어주도록 변경 : DI ( Dependency Injection )
-        this.memoryMemberRepository = memoryMemberRepository;
+    public MemberService(MemberRepository memberRepository) {
+        // new 를 통해서 생성하는게 아니라 외부에서 넣어주도록 변경 : DI ( Dependency Injection ) -> 생성자 주입 권장 ( 필드 주입, setter 주입 지양 )
+        this.memberRepository = memberRepository;
     }
-
 
     public Long join ( Member member) {     // 회원가입
         validateDuplicateMember(member);    // 같은 이름이 있는 회원은 가입 불가
 
-        memoryMemberRepository.save( member );
+        memberRepository.save( member );
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
-        Optional<Member> result = memoryMemberRepository.findByName(member.getName());
+        Optional<Member> result = memberRepository.findByName(member.getName());
         result.ifPresent(m -> {
             throw new IllegalStateException("이미 존재하는 회원입니다.");
         });
     }
 
     public List<Member> findMembers() {         // 전체 회원 조회
-        return memoryMemberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId) {
-        return memoryMemberRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 }
